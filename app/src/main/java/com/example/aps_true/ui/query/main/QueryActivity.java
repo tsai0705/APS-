69dee98336e8017ab88c9f77e5bd4a05e7fbb4c1
feng <yizhen200525@gmail.com>
2025-10-23 17:48:42 +0800

    [feat] api進度表查詢完成

--- app/src/main/java/com/example/aps_true/ui/query/main/QueryActivity.java
+++ app/src/main/java/com/example/aps_true/ui/query/main/QueryActivity.java
@@ -45,10 +45,10 @@ public class QueryActivity extends AppCompatActivity{
     private TabData tabData = TabData.getInstance();
     private ApiClient apiClient;
     private GetApi getApi;
-    private String soid = "";
-    private String clientname = "";
-//    private ArrayList<String> soIdList = new ArrayList<>();
-
+    private String[] soIds = new String[0];
+    private String[] customers = new String[0];
+    private ArrayList<String> clientName = new ArrayList<>();
+    private ArrayList<String> so = new ArrayList<>();
     private int choice = 0;
 
     @Override
@@ -61,6 +61,7 @@ public class QueryActivity extends AppCompatActivity{
         usernameTextview.setText(loginData.getName());
 
         bindUI();
+        getOrderData();
 
         final String[] str = {"點焊"};
         ArrayAdapter<String> strList = new ArrayAdapter<>(
@@ -90,6 +91,7 @@ public class QueryActivity extends AppCompatActivity{
                 finish();
             }
         });
+
     }
 
     protected void bindUI(){
@@ -108,10 +110,7 @@ public class QueryActivity extends AppCompatActivity{
 
     }
 
-    protected void orderclick(View view) {
-        choice = 0;
-        AlertDialog.Builder dialog = new AlertDialog.Builder(QueryActivity.this);
-
+    protected void getOrderData(){
         String so_id = "";
         getApi.getOrder(so_id, loginData.getToken())
                 .subscribeOn(Schedulers.io())
@@ -123,7 +122,6 @@ public class QueryActivity extends AppCompatActivity{
                             Log.d("getOrder", "收到訂單: " + order.getSoId());
                         }
 
-                        ArrayList<String> so = new ArrayList<>();
                         String ordernumber = ordernumberEditText.getText().toString();
                         if (ordernumber != null && !ordernumber.isEmpty()){
                             // order是responseList裡的每個 OrderResponse 物件
@@ -145,7 +143,8 @@ public class QueryActivity extends AppCompatActivity{
                         }
 
                         // 轉成字串陣列給 Dialog 用
-                        String[] soIds = so.toArray(new String[0]);
+                        soIds = so.toArray(new String[0]);
+                        getClientData();
 
                         // 如果沒有任何 soId，顯示提示
                         if (soIds.length == 0) {
@@ -155,25 +154,6 @@ public class QueryActivity extends AppCompatActivity{
                                     .show();
                             return;
                         }
-
-
-                        //宣告一個 reference(choiceHolder) 不能變的 int 陣列（內容可以變）
-                        final int[] choiceHolder = {choice};
-                        // choiceHolder[0] : 預設選項
-                        // (dialogInterface, i) -> choiceHolder[0] = i：當使用者選擇其中一項時，把 index 存進 choiceHolder[0]
-                        dialog.setSingleChoiceItems(soIds, choiceHolder[0], (dialogInterface, i) -> choiceHolder[0] = i);
-
-                        dialog.setPositiveButton("確定", (dialogInterface, i) -> {
-                            ordernumberEditText.setText(soIds[choiceHolder[0]]);
-                            ArrayList<String> soid = new ArrayList<>();
-                            soid.add(soIds[choiceHolder[0]]);
-                            ArrayList<String> soClear = new ArrayList<>();
-                            tabData.setSo(soClear);
-                            tabData.setSo(soid);
-                        });
-                        ArrayList<String> soIdList = tabData.getSo();
-                        Log.d("getSo", "so size=" + soIdList.size());
-                        dialog.show();
                     }
 
                     @Override
@@ -186,14 +166,9 @@ public class QueryActivity extends AppCompatActivity{
                         Log.d("getOrder", "請求完成");
                     }
                 });
-
     }
 
-    // 客戶
-     protected void clientclick(View view){
-        choice = 0;
-        AlertDialog.Builder dialog = new AlertDialog.Builder(QueryActivity.this);
-
+    protected void getClientData() {
         String customer_name = "";
         getApi.getCustomer(customer_name, loginData.getToken())
                 .subscribeOn(Schedulers.io())
@@ -201,66 +176,111 @@ public class QueryActivity extends AppCompatActivity{
                 .subscribe(new DisposableObserver<List<CustomerResponse>>() {
                     @Override
                     public void onNext(List<CustomerResponse> responseList) {
-                        for (CustomerResponse customer : responseList) {
-                            Log.d("getCustomer", "收到訂單: " + customer.getCustomer_name());
-                        }
-
-                        ArrayList<String> clientName = new ArrayList<>();
+                        Log.d("getCustomer", "收到 " + responseList.size() + " 筆客戶資料");
+                        clientName.clear();
                         String client = clientEditText.getText().toString();
-                        if (client != null && !client.isEmpty()){
-                            for (CustomerResponse customer : responseList ) {
+
+                        if (client != null && !client.isEmpty()) {
+                            Log.d("getCustomer", "客戶名稱包含 '" + client + "'");
+                            for (CustomerResponse customer : responseList) {
                                 String customerName = customer.getCustomer_name();
-                                if (customerName.contains(client)){
+
+                                if (customerName != null && customerName.contains(client)) {
+                                    if (!clientName.contains(customerName)) {
+                                        clientName.add(customerName);
+                                        Log.d("getCustomer", "符合: " + customerName);
+                                    }
+                                }
+                            }
+                        }
+                        // 沒有輸入：顯示全部
+                        else {
+                            Log.d("getCustomer", "顯示全部客戶");
+
+                            for (CustomerResponse customer : responseList) {
+                                String customerName = customer.getCustomer_name();
+                                if (customerName != null && !customerName.isEmpty()) {
                                     clientName.add(customerName);
                                 }
                             }
-                        }else {
-                            for (CustomerResponse customer : responseList ) {
-                                String customerName = customer.getCustomer_name();
-                                clientName.add(customerName);
-                            }
-                            // 全部添加
-                            tabData.setCustomer(clientName);
                         }
 
-                        // 轉成字串陣列給 Dialog 用
-                        String[] customers = clientName.toArray(new String[0]);
+                        tabData.setCustomer(clientName);
+                        Log.d("getCustomer", "已設定 " + clientName.size() + " 筆客戶到 TabData");
+                        customers = clientName.toArray(new String[0]);
 
-                        // 如果沒有任何customer，顯示提示
                         if (customers.length == 0) {
-                            new AlertDialog.Builder(QueryActivity.this)
-                                    .setMessage("查無符合的客戶名稱！")
-                                    .setPositiveButton("確定", null)
-                                    .show();
-                            return;
+                            Log.w("getCustomer", "查無符合的客戶");
+                            runOnUiThread(() -> {
+                                new AlertDialog.Builder(QueryActivity.this)
+                                        .setMessage("查無符合的客戶名稱！")
+                                        .setPositiveButton("確定", null)
+                                        .show();
+                            });
+                        } else {
+                            Log.d("getCustomer", "查詢完成: " + customers.length + " 筆客戶");
                         }
-
-                        final int[] choiceHolder = {choice};
-                        dialog.setSingleChoiceItems(customers, choice, (dialogInterface, i) -> choice = i);
-
-                        dialog.setPositiveButton("確定", (dialogInterface, i) -> {
-                            clientEditText.setText(customers[choiceHolder[0]]);
-                            ArrayList<String> clientname = new ArrayList<>();
-                            clientname.add(customers[choiceHolder[0]]);
-                            ArrayList<String> customerClear = new ArrayList<>();
-                            tabData.setCustomer(customerClear);
-                            tabData.setCustomer(clientname);
-                        });
-                        ArrayList<String> customerList = tabData.getCustomer();
-                        Log.d("getCustomer", "customer size=" + customerList.size());
-                        dialog.show();
                     }
 
                     @Override
                     public void onError(Throwable e) {
-                        Log.e("getCustomer", "API 錯誤: ", e);
+                        Log.e("getCustomer", "API 錯誤", e);
+                        runOnUiThread(() -> {
+                            new AlertDialog.Builder(QueryActivity.this)
+                                    .setMessage("載入客戶失敗: " + e.getMessage())
+                                    .setPositiveButton("確定", null)
+                                    .show();
+                        });
                     }
 
                     @Override
                     public void onComplete() {
-                        Log.d("getCustomer", "請求完成");
                     }
                 });
+    }
+
+    protected void orderclick(View view) {
+        choice = 0;
+        AlertDialog.Builder dialog = new AlertDialog.Builder(QueryActivity.this);
+
+        //宣告一個 reference(choiceHolder) 不能變的 int 陣列（內容可以變）
+        final int[] choiceHolder = {choice};
+        // choiceHolder[0] : 預設選項
+        // (dialogInterface, i) -> choiceHolder[0] = i：當使用者選擇其中一項時，把 index 存進 choiceHolder[0]
+        dialog.setSingleChoiceItems(soIds, choiceHolder[0], (dialogInterface, i) -> choiceHolder[0] = i);
+
+        dialog.setPositiveButton("確定", (dialogInterface, i) -> {
+            ordernumberEditText.setText(soIds[choiceHolder[0]]);
+            ArrayList<String> soid = new ArrayList<>();
+            soid.add(soIds[choiceHolder[0]]);
+            ArrayList<String> soClear = new ArrayList<>();
+            tabData.setSo(soClear);
+            tabData.setSo(soid);
+        });
+        ArrayList<String> soIdList = tabData.getSo();
+        Log.d("getSo", "so size=" + soIdList.size());
+        dialog.show();
+
+    }
+
+    protected void clientclick(View view){
+        choice = 0;
+        AlertDialog.Builder dialog = new AlertDialog.Builder(QueryActivity.this);
+
+        final int[] choiceHolder = {choice};
+        dialog.setSingleChoiceItems(customers, choice, (dialogInterface, i) -> choice = i);
+
+        dialog.setPositiveButton("確定", (dialogInterface, i) -> {
+            clientEditText.setText(customers[choiceHolder[0]]);
+            ArrayList<String> clientname = new ArrayList<>();
+            clientname.add(customers[choiceHolder[0]]);
+            ArrayList<String> customerClear = new ArrayList<>();
+            tabData.setCustomer(customerClear);
+            tabData.setCustomer(clientname);
+        });
+        ArrayList<String> customerList = tabData.getCustomer();
+        Log.d("getCustomer", "customer size=" + customerList.size());
+        dialog.show();
     }
 
     protected void dateclick(View view){
